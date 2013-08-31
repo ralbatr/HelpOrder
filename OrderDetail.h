8fbaaa60adc6ac5c40e49042976ef555f460b283
ralbatr <ralbatr@gmail.com>
2013-08-31 17:57:50 +0000

    第三次上传 订餐功能完成

---
+++ OrderDetail.h
@@ -0,0 +1,13 @@
+//
+//  OrderDetail.h
+//  HelpOrder
+//
+//  Created by Ralbatr on 13-8-31.
+//  Copyright (c) 2013年 Ralbatr. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OrderDetail : NSObject
+
+@end
---
+++ OrderDetail.m
@@ -0,0 +1,13 @@
+//
+//  OrderDetail.m
+//  HelpOrder
+//
+//  Created by Ralbatr on 13-8-31.
+//  Copyright (c) 2013年 Ralbatr. All rights reserved.
+//
+
+#import "OrderDetail.h"
+
+@implementation OrderDetail
+
+@end
