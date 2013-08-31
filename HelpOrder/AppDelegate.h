fe0363f547ce64b9bb6e627ba8562a559675da31
ralbatr <ralbatr@gmail.com>
2013-08-31 14:55:26 +0000

    Initial Commit

---
+++ HelpOrder/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelpOrder
+//
+//  Created by Ralbatr on 13-8-31.
+//  Copyright (c) 2013年 Ralbatr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
