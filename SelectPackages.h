0a12a9682dc27ac6ea0499d5caa903f7cf5dd4ac
ralbatr <ralbatr@gmail.com>
2013-09-05 15:37:48 +0000

    第九次上传 把长代码分开

---
+++ SelectPackages.h
@@ -0,0 +1,13 @@
+//
+//  SelectPackages.h
+//  HelpOrder
+//
+//  Created by Ralbatr on 13-9-5.
+//  Copyright (c) 2013年 Ralbatr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SelectPackages : UIView
+
+@end
---
+++ SelectPackages.m
@@ -0,0 +1,31 @@
+//
+//  SelectPackages.m
+//  HelpOrder
+//
+//  Created by Ralbatr on 13-9-5.
+//  Copyright (c) 2013年 Ralbatr. All rights reserved.
+//
+
+#import "SelectPackages.h"
+
+@implementation SelectPackages
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
